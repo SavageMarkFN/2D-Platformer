e2a7b3f965e95cc516eaf5936061f1edf8bf0747
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2024-07-04 14:51:37 +0300

    Last Set up

--- 2D-Platformer/Assets/MyCode.cs
+++ 2D-Platformer/Assets/MyCode.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class MyCode : MonoBehaviour
 {
+    #region Basics
+    /*
+     Attribute private-public
+     Variable Type int-float-bool-string-GameObject
+     Create a variable-->Attribute + Variable Type + Name + ;
+     */
+
+    [Header("Variabbles")]
+    private bool Open = true;
+    public float Health;
+    private string Name = "Door";
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
