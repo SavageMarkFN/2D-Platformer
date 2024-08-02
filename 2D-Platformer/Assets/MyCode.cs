1d6b71b8ac1e46ea1ac41225ac33dd3da704363c
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2024-08-02 17:26:42 +0300

    Students Folder

--- 2D-Platformer/Assets/MyCode.cs
+++ 2D-Platformer/Assets/MyCode.cs
@@ -10,10 +10,10 @@ public class MyCode : MonoBehaviour
      Variable Type int-float-bool-string-GameObject
      Create a variable-->Attribute + Variable Type + Name + ;
      */
-
-    [Header("Variabbles")]
+    [Header("Basic Variables")]
+    public int Number = 1;
+    public float Health = 4.2f;
     private bool Open = true;
-    public float Health;
     private string Name = "Door";
     #endregion
 
