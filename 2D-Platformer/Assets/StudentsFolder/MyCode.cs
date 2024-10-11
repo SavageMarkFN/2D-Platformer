6c2959b09e3f415617cc602fc07f34a3a0f842ae
IndieTeacher <UnitysEducation@gmail.com>
2024-10-11 19:35:40 +0300

    Changes

--- 2D-Platformer/Assets/StudentsFolder/MyCode.cs
+++ 2D-Platformer/Assets/StudentsFolder/MyCode.cs
@@ -1,43 +1,18 @@
-//Libraries
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Code
 public class MyCode : MonoBehaviour
 {
-    #region Variables
-    /*
-    Attributes
-     private --> Only the script that contains the variable have access to change the value
-     public  --> All scripts have access to this variable to change the value
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    Variable Types
-     int       > Takes all integer numbers
-     float     > Takes all the numbers
-     bool      > true/false
-     string    > "Text"
-     GameObject> Variable that contains an object in the scene
-     We can enable and disable this object with the ObjectName.SetActive(true/false);
-     Transform > Variable that contains the Position,Rotation,Scale
-     */
-    private int Number = 5;
-    public float _Number = -2.5f;
-    private bool Check = true;
-    public string Item = "Sword";
-    public GameObject Chest;
-    private Transform NewPosition;
-    #endregion
-
-    #region Single if Condition
-
-    #endregion
-
-    #region Multiple If Conditions
-
-    #endregion
-
-    #region Collisions
-
-    #endregion
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
