482e6866bea009dd22636d241a6f740e25a12170
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2024-10-19 08:01:26 +0300

    New Assets

--- 2D-Platformer/Assets/StudentsFolder/MyCode.cs
+++ 2D-Platformer/Assets/StudentsFolder/MyCode.cs
@@ -19,6 +19,8 @@ public class MyCode : MonoBehaviour
     private = A variable that can be accessed from this script
     public  = A variable that can be accessed from all the scripts
     */
+
+    //Examples
     public int Number = 10;
     private float Health = 100;
     public bool Check = true;
@@ -28,7 +30,39 @@ public class MyCode : MonoBehaviour
     #endregion
 
     #region Single If
+    /*
+    The if statement allows us to make decisions based on certain condition
+    Basics conditions
+    Equals to        (==)
+    Higher than      (>)
+    Lower  than      (<)
+    Higher and equal (>=)
+    Lower and equal  (<=)
+    not equal        (!=)
+     */
 
+    //Examples
+    void Example()
+    {
+        int Number = 7;
+
+        if (Number > 10)
+            Debug.Log("Number is higher than 10");
+        else if (Number < 10)
+            Debug.Log("Number is smaller to 10");
+        else if (Number > 5)
+            Debug.Log("Number is higher than 5");
+        else
+            Debug.Log("Number is smaller than 5");
+/////////////////////////////////////////////////////////////////////////////////////////
+        bool AttackPlayer = false;
+        float Distance = 0;
+
+        if (Distance >= 15)
+            AttackPlayer = false;
+        else
+            AttackPlayer = true;
+    }
     #endregion
 
     #region Multiple If
@@ -40,7 +74,18 @@ public class MyCode : MonoBehaviour
     #endregion
 
     #region Animator
+    /*
+    In order to change a variable inside to an animator we need to Set the variable
+    Animator TestAnimator;
+    TestAnimator.SetInteger("Number", 10);
+    TestAnimator.SetFloat("Number", 1.1);
+    TestAnimator.SetBool("Locked", true);
 
+    A Trigger Variable in animator is  a bool that is reset by the controller when consumed by a transition 
+    (When you change an animation state with her as a condition)
+    TestAnimator.SetTrigger("Open");   //It turns the Open to true
+    TestAnimator.ResetTrigger("Open"); //It turns the Open back to false)
+     */
     #endregion
 
     #region Collision
