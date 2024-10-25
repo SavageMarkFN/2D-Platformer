e88ac5d3f3edeb98633ecbe2dba8235318ecce70
IndieTeacher <UnitysEducation@gmail.com>
2024-10-25 20:39:32 +0300

    Assets

--- 2D-Platformer/Assets/StudentsFolder/MyCode.cs
+++ 2D-Platformer/Assets/StudentsFolder/MyCode.cs
@@ -62,19 +62,87 @@ public class MyCode : MonoBehaviour
     #endregion
 
     #region Multiple If
-    
+    // (&& And)
+    // (|| Or)
+
+    void MultipleIf()
+    {
+        float Health = 75;
+        string Condition;
+        bool Dead = false;
+
+        if (Health > 100)
+            Condition = "Very well";
+        else if (Health > 50 && Health < 75)
+            Condition = "Good";
+        else if (Health > 25 && Health < 50)
+            Condition = "Okay";
+        else if (Health < 25)
+            Condition = "Bad";
+
+        if (Health <= 0 && Dead == false)
+            Dead = true;
+    }
     #endregion
 
     #region For Loop
+    /*
+    int i  > How many times i repeat the code
+    Length > How many times i need to repeat the code
+    i++    > Increaser
+    break; > Stops the for
+    */
 
+    public void ForLoopExample()
+    {
+        int Number = 0;
+        int LoopTimes = 10;
+
+        for (int i = 0; i < LoopTimes; i++)
+        {
+            Number += 5;
+        }
+//////////////////////////////////////////////////////////////////
+
+        float Health = 0;
+        LoopTimes = 35;
+
+        for (int i = 0; i < LoopTimes; i++)
+        {
+            Health += 2.5f;
+
+            if (Health > 100)
+                break;
+        }
+    }
     #endregion
-
+ 
     #region Animator
+    Animator animator; //The name of the animator
+    void AnimatorController()
+    {
+        //How to change the variables of an animator
+        animator.SetInteger("Number", 10);
+        animator.SetFloat("Number", 2.6f);
+        animator.SetBool("Open", true);
+        animator.SetTrigger("Attack");   //Sets the trigger to true
+        animator.ResetTrigger("Attack"); //Sets the trigger to false
 
+        //How to get variables from the animator
+        int Number = animator.GetInteger("Number");
+        float State = animator.GetFloat("State");
+        bool Open = animator.GetBool("Open");
+
+        //How to change animator's speed
+        animator.speed = 1;
+
+        //How to enable/disable the animator
+        animator.enabled = false; //true > enables the animator
+    }
     #endregion
 
     #region Collision
-
+    
     #endregion
 
     #region Mouse and Keyboard Inputs
@@ -93,3 +161,8 @@ public class MyCode : MonoBehaviour
         
     }
 }
+
+/*
+Health - Mana Potion
+Golden - Silver - Diamon Key
+*/
