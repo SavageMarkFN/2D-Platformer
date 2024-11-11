ec97916d892212889fed2d9cb555cfe7a52c94c2
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2024-11-11 18:37:49 +0200

    Chest

---
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyChest.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyChest : MonoBehaviour
+{
+    private bool InRange;
+    public GameObject Message;
+    public string Item;
+    public InventoryController IC;
+
+    private void OnTriggerEnter2D(Collider2D Object)
+    {
+      if(Object.tag == "Player")
+        {
+            InRange = true;
+            Message.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D Object)
+    {
+        if (Object.tag == "Player")
+        {
+            InRange = false;
+            Message.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.E) && InRange == true )
+      {
+          InRange = false;
+          Message.SetActive(false);
+          IC.AddItem(Item);
+          this.gameObject.SetActive(false);
+      }       
+    }
+}
