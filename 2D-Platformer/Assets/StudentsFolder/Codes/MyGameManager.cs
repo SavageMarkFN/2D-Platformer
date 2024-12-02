86c802e86bb5055820674118504111f9c682dbd5
IndieTeacher <UnitysEducation@gmail.com>
2024-12-02 20:44:55 +0200

    NewUI

--- 2D-Platformer/Assets/StudentsFolder/Codes/MyGameManager.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyGameManager.cs
@@ -7,12 +7,18 @@ using TMPro;
 public class MyGameManager : MonoBehaviour
 {
     #region Player Variables
+    private int UIOpened; //0 = None, 1 = Pause Menu, 2 = Inventory, 3 = Something else
+
+    [Header("References")]
     private PlayerMovement PM;
 
     [Header("Player UI")]
     public Image PlayerIcon;
     public Slider[] PlayerSlider; //0 = Health, 1 = Mana, 2 = Stamina, 3 = XP
     public TextMeshProUGUI GoldText;
+
+    [Header("Inventory")]
+    public GameObject InventoryObject;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -24,6 +30,7 @@ public class MyGameManager : MonoBehaviour
     void Update()
     {
         SetPlayerUI();
+        InventoryManager();
     }
 
     #region Player UI
@@ -37,6 +44,40 @@ public class MyGameManager : MonoBehaviour
         PlayerSlider[1].value = PM.Mana;
         PlayerSlider[2].maxValue = PM.MaxStamina;
         PlayerSlider[2].value = PM.Stamina;
+    }
+    #endregion
+
+    #region Inventory Manager
+    void InventoryManager()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (UIOpened == 0)
+            {
+                PM.CanAttack = false;
+                InventoryObject.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                UIOpened = 2;
+            }
+            else if (UIOpened == 2)
+            {
+                PM.CanAttack = true;
+                InventoryObject.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                UIOpened = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && UIOpened == 2)
+        {
+            PM.CanAttack = true;
+            InventoryObject.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            UIOpened = 0;
+        }
     }
     #endregion
 }
