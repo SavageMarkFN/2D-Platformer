b8a79123ebd0099fd777e5d3c122398d47846379
KG_Zanie <77167473+NLNemesis@users.noreply.github.com>
2025-01-08 16:55:04 +0200

    Set

---
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyMerchant.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMerchant : MonoBehaviour
+{
+    //One Background with Frame 1920 x 1080 (192 x 108) (Shop_Background)
+    //One Background 200 x 250 (20 x 25) (ShopItem_Background)
+    //One Frame 200 x 250 (20 x 25) (ShopItem_Frame)
+    //One Button 160 x 30 (16x3) (Buy Button)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
