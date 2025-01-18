a8e099702bf2998cd83a46a38ee9ad8434a681db
IndieTeacher <UnitysEducation@gmail.com>
2025-01-18 10:11:39 +0200

    Set

--- 2D-Platformer/Assets/StudentsFolder/Codes/MyMerchant.cs
+++ 2D-Platformer/Assets/StudentsFolder/Codes/MyMerchant.cs
@@ -5,48 +5,5 @@ using UnityEngine.Events;
 
 public class MyMerchant : MonoBehaviour
 {
-    #region Variables
-    [Header("Interaction")]
-    public GameObject Message;
-    private bool InRange;
-
-    [Header("References")]
-    public UnityEvent OpenShopEvent;
-    public UnityEvent CloseShopEvent;
-    #endregion
-
-    #region On Trigger
-    void OnTriggerStay2D(Collider2D Object)
-    {
-        if (Object.name == "Player")
-        {
-            InRange = true;
-            Message.SetActive(true);
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D Object)
-    {
-        if (Object.name == "Player")
-        {
-            InRange = false;
-            Message.SetActive(false);
-        }
-    }
-    #endregion
-
-    void Update()
-    {
-        if (InRange)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-                OpenShopEvent.Invoke();
-
-            if (Input.GetKeyDown(KeyCode.Escape))
-                CloseShopEvent.Invoke();
-
-            if (Input.GetKeyDown(KeyCode.E) && Message.activeSelf == false)
-                CloseShopEvent.Invoke();
-        }   
-    }
+    
 }
